fd70158f97143cea18d2ec1bf1527a00c2192fad
Chris Piraino <cpiraino@hashicorp.com>
2020-03-26 16:51:18 -0500

    Add support for ingress-gateway in CLI command
    - Supports -register command
    - Creates a static Envoy listener that exposes only the /ready API so
    that we can register a TCP healthcheck against the ingress gateway
    itself
    - Updates ServiceAddressValue.String() to be more in line with Value()

--- command/connect/envoy/envoy.go
+++ command/connect/envoy/envoy.go
@@ -77,6 +77,7 @@ const (
 var supportedGateways = map[string]api.ServiceKind{
 	"mesh":        api.ServiceKindMeshGateway,
 	"terminating": api.ServiceKindTerminatingGateway,
+	"ingress":     api.ServiceKindIngressGateway,
 }
 
 func (c *cmd) init() {
@@ -214,6 +215,7 @@ func (c *cmd) Run(args []string) int {
 		c.UI.Error("The mesh-gateway flag is deprecated and cannot be used alongside the gateway flag")
 		return 1
 	}
+
 	if c.meshGateway {
 		c.gateway = meshGatewayVal
 	}
@@ -233,7 +235,7 @@ func (c *cmd) Run(args []string) int {
 	if c.gateway != "" {
 		kind, ok := supportedGateways[c.gateway]
 		if !ok {
-			c.UI.Error("Gateway must be one of: terminating or mesh")
+			c.UI.Error("Gateway must be one of: terminating, mesh, or ingress")
 			return 1
 		}
 		c.gatewayKind = kind
@@ -523,6 +525,17 @@ func (c *cmd) generateConfig() ([]byte, error) {
 	}
 
 	var bsCfg BootstrapConfig
+
+	// Setup ready listener for ingress gateway to pass healthcheck
+	if c.gatewayKind == api.ServiceKindIngressGateway {
+		lanAddr := c.lanAddress.String()
+		// Deal with possibility of address not being specified and defaulting to
+		// ":443"
+		if strings.HasPrefix(lanAddr, ":") {
+			lanAddr = "127.0.0.1" + lanAddr
+		}
+		bsCfg.ReadyBindAddr = lanAddr
+	}
 
 	if !c.disableCentralConfig {
 		// Fetch any customization from the registration
